b93e6eec5ed1d07d330b887e07ef0939009a072e
명직 <44960073+LeeMyungJic@users.noreply.github.com>
2021-03-20 20:36:30 +0900

    타겟넘버 - 테스트케이스 7 통과

--- TargetNumber.java
+++ TargetNumber.java
@@ -1,49 +1,40 @@
 package Programmers;
 
-public class TargetNumber {
+import java.util.LinkedList;
+import java.util.Queue;
 
-    static boolean[] visit;
-    static int[] numberClone;
-    static int baseDist;
-    static int baseSum;
-    static int result = 0;
+public class TargetNumber {
     public static void main(String[] args) {
-        int[] numbers = {1, 1, 1, 1, 1};
+        int[] numbers = {1,1,1,1,1};
         int target = 3;
-        System.out.println(solution(numbers, target));
+        solution(numbers, target);
     }
     public static int solution(int[] numbers, int target) {
         int answer = 0;
-        baseDist = numbers.length;
-        visit = new boolean[baseDist];
-        numberClone = new int[baseDist];
-        baseSum = target;
-        numberClone = numbers;
-        dfs(0, 0, 0);
-        answer = result;
-        return answer;
-    }
 
-    public static void dfs(int dist, int cur, int sum) {
-        if(dist == baseDist) {
-            if(sum == baseSum) {
-                result++;
-            }
-        }
-        else {
-            for(int i = cur; i < baseDist; i++) {
-                if(!visit[i]) {
-                    visit[i] = true;
-                    for(int j = 0; j < 2; j++) {
-                        int signTemp = numberClone[i];
-                        if(j == 1) {
-                            signTemp = -numberClone[i];
-                        }
-                        dfs(dist + 1, i, sum + signTemp);
-                    }
-                    visit[i] = false;
+        Queue<Nums> qu = new LinkedList<>();
+        qu.add(new Nums(numbers[0], 1));
+        qu.add(new Nums(-numbers[0], 1));
+        while (!qu.isEmpty()) {
+            Nums n = qu.poll();
+            if(n.dist == numbers.length) {
+                if(n.sum == target) {
+                    answer++;
                 }
             }
+            else {
+                qu.add(new Nums(n.sum + numbers[n.dist], n.dist+1));
+                qu.add(new Nums(n.sum - numbers[n.dist], n.dist+1));
+            }
         }
+        return answer;
+    }
+}
+class Nums {
+    int sum;
+    int dist;
+    Nums(int sum, int dist) {
+        this.sum = sum;
+        this.dist = dist;
     }
 }
